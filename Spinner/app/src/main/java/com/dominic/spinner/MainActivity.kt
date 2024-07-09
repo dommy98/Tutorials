e0db259b67e2a988814cc1ec8e386eaeded8a03a
dominickiplimo <dominickiplimo2017@gmail.com>
2024-07-09 14:56:44 +0300

    Spinner

---
+++ Spinner/app/src/main/java/com/dominic/spinner/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.dominic.spinner
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var spinner: Spinner
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        spinner = findViewById(R.id.spinner)
+        val listItems = listOf("Chocolate", "Strawberry", "Vanilla", "Mango")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listItems)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = arrayAdapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@MainActivity, "You have selected $selectedItem flavour",Toast.LENGTH_SHORT ).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+    }
+}
