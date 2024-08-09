ab43468bf1c89744c08e01e8078ed823a834183c
dominickiplimo <dominickiplimo2017@gmail.com>
2024-08-09 12:17:35 +0300

    signup logic

--- SignUp and Login/app/src/main/java/com/dominic/signupandlogin/fragments/SignInFragment.kt
+++ SignUp and Login/app/src/main/java/com/dominic/signupandlogin/fragments/SignInFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
 import com.dominic.signupandlogin.R
+import com.dominic.signupandlogin.databinding.FragmentSignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInFragment : Fragment() {
 
+    private lateinit var binding: FragmentSignInBinding
+    private lateinit var navControl: NavController
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 
--- SignUp and Login/app/src/main/java/com/dominic/signupandlogin/fragments/SignUpFragment.kt
+++ SignUp and Login/app/src/main/java/com/dominic/signupandlogin/fragments/SignUpFragment.kt
@@ -5,17 +5,64 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.dominic.signupandlogin.R
+import com.dominic.signupandlogin.databinding.FragmentSignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var navControl : NavController
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    binding = FragmentSignUpBinding.inflate(inflater, container, false)
+    return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+        registerEvents()
+    }
+
+    private fun registerEvents() {
+
+        binding.authTextView.setOnClickListener{
+            navControl.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+      binding.nextBtn.setOnClickListener {
+          val email = binding.emailEt.text.toString()
+          val pass  = binding.passEt.text.toString()
+          val verifyPass = binding.rePassEt.text.toString()
+
+          if (email.isNotEmpty() && pass.isNotEmpty() && verifyPass.isNotEmpty()){
+              if (pass == verifyPass){
+                  auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                      if (it.isSuccessful){
+                          Toast.makeText(context, "Registered Successfully", Toast.LENGTH_SHORT).show()
+                          navControl.navigate(R.id.action_signUpFragment_to_homeFragment)
+                      }else{
+                          Toast.makeText(context,it.exception?.message, Toast.LENGTH_SHORT).show()
+                      }
+                  }
+              }
+          }
+      }
+    }
+
+    private fun init(view: View) {
+        navControl= Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+
+    }
 
 }
