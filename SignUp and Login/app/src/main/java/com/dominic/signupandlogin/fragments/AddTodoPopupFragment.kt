d6eb7d799eb501fba68f01586d0afc40c6d25117
dominickiplimo <dominickiplimo2017@gmail.com>
2024-08-19 15:29:26 +0300

    home fraf

---
+++ SignUp and Login/app/src/main/java/com/dominic/signupandlogin/fragments/AddTodoPopupFragment.kt
@@ -0,0 +1,50 @@
+package com.dominic.signupandlogin.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.dominic.signupandlogin.R
+import com.dominic.signupandlogin.databinding.FragmentAddTodoPopupBinding
+import com.google.android.material.textfield.TextInputEditText
+
+class AddTodoPopupFragment : DialogFragment() {
+
+    private lateinit var binding: FragmentAddTodoPopupBinding
+    private lateinit var listener: DialogNextBtnClickListener
+
+    fun setListener(listener: DialogNextBtnClickListener){
+        this.listener = listener
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddTodoPopupBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        registerEvents()
+    }
+
+    private fun registerEvents() {
+        binding.todoNextBtn.setOnClickListener {
+            val todoTask = binding.todoEt.text.toString()
+            if (todoTask.isNotEmpty()){
+                listener.onSaveTask(todoTask, binding.todoEt)
+            }else{
+                Toast.makeText(context, "Please add a task", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    interface DialogNextBtnClickListener{
+        fun onSaveTask(todo : String, todoEt : TextInputEditText)
+    }
+}
--- SignUp and Login/app/src/main/java/com/dominic/signupandlogin/fragments/HomeFragment.kt
+++ SignUp and Login/app/src/main/java/com/dominic/signupandlogin/fragments/HomeFragment.kt
@@ -5,16 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.dominic.signupandlogin.R
+import com.dominic.signupandlogin.databinding.FragmentHomeBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), AddTodoPopupFragment.DialogNextBtnClickListener {
 
+    private lateinit var navControl: NavController
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var popupFragment: AddTodoPopupFragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,  container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+        registerEvents()
+    }
+
+    private fun registerEvents() {
+        binding.addBtnHome.setOnClickListener {
+            popupFragment = AddTodoPopupFragment()
+            popupFragment.setListener(this)
+            popupFragment.show(
+                childFragmentManager,
+                "AddTodoPopupFragment"
+
+            )
+        }
+    }
+
+    private fun init(view: View) {
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+    }
+
+    override fun onSaveTask(todo: String, todoEt: TextInputEditText) {
+        TODO("Not yet implemented")
     }
 
 
